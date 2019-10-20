f12179008bf9a6f272895dfdc4d82223e3b3ea0f
Torleif Hensvold <torleif.hensvold@outlook.com>
2019-10-20 16:37:07 +0200

    Moved the navBar JSON to addNavBar.js because problems in reading location
    The location of the JSON file was difficult to read locally in the
    development environment, and thus it was easier to add the JSON as a
    const to the addNavBar.js
    
    I have also tried to add buttons to the navBar with the links, might need
    some help with making that work, or simply to let it mature in my head.

--- Hall's Kitchen/scripts/addNavBar.js
+++ Hall's Kitchen/scripts/addNavBar.js
@@ -1,6 +1,110 @@
+const navBar = document.getElementById('navBar')
+navBar.style.width = '100%'
+navBar.style.height = '50px'
+navBar.style.backgroundColor = 'grey'
 
+/*
+	A JSON object that represents the pages we have on the site, with
+	relative linking.
+ */
+const pagesJSON =
+	{
+		'mainpage':
+			{
+				'name': 'Main Page',
+				'url': 'index.html',
+			}
+		,
+		'menu':
+			{
+				'name': 'Menu',
+				'url': 'menu.html',
+			}
+		,
+		'cart':
+			{
+				'name': 'Cart',
+				'url': 'cart.html',
+			}
+		,
+		'contact_us':
+			{
+				'name': 'Contact us',
+				'url': 'contact_us.html',
+			}
+		,
+		'about_us':
+			{
+				'name': 'About us',
+				'url': 'about_us.html',
+			},
+	}
 
-const navBar = document.getElementById("navBar")
-navBar.style.width = "100%";
-navBar.style.height = "50px";
-navBar.style.backgroundColor = "black";
+/**
+ * @return {Array} An array of links
+ */
+function createButtonOrder (JSONObject)
+{
+	let linkArray = []
+	for (let key in JSONObject)
+	{
+		let url = ''
+		let name = ''
+		let subObject = JSONObject[key]
+		for (let subkey in subObject)
+		{
+			if (subkey === 'name')
+			{
+				let subSubObject = subObject[subkey]
+				//console.log("\t" + subkey + "\t" + subSubObject)
+				name = subSubObject
+			}
+			if (subkey === 'url')
+			{
+				let subSubObject = subObject[subkey]
+				//console.log("\t" + subkey + "\t" + subSubObject)
+				url = subSubObject
+			}
+		}
+		let link = createLink(url, name)
+		linkArray.push(link)
+	}
+	//console.log(linkArray)
+	return linkArray
+}
+
+/**
+ *
+ * @param url A relative link URL for use in "a href"
+ * @param text The text that should show for the URL
+ * @return {HTMLElement} A link with text.
+ */
+function createLink (url, text)
+{
+	let a = document.createElement('a')
+	a.href = url;
+	a.innerHTML = text;
+	a.className = "navButton";
+	//console.log(a);
+	return a
+}
+
+function addButtons ()
+{
+	let request = new XMLHttpRequest()
+	request.open('GET', 'JSON_files/navBar', false) // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/open
+	request.send()
+	let JSONObject = JSON.parse(request.responseText)
+	console.log(JSONObject)
+	for (let site in JSONObject)
+	{
+		console.log(site)
+	}
+}
+
+const urlArray = createButtonOrder(pagesJSON)
+
+for (let url in urlArray)
+{
+	navBar.innerText.concat(url);
+}
