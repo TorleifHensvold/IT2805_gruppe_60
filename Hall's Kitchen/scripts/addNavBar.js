80fe40f0e73adeb71d47941ddda7fd9079bb86cb
unknown <lukasnt@stud.ntnu.no>
2019-10-30 15:10:56 +0100

    Some comments in addNavBar.js

--- Hall's Kitchen/scripts/addNavBar.js
+++ Hall's Kitchen/scripts/addNavBar.js
@@ -1,3 +1,4 @@
+// Array of all the pages
 const pages = [
     {name: "Main Page", file: "index.html"},
     {name: "Menu", file: "menu.html"},
@@ -6,8 +7,8 @@ const pages = [
     {name: "Contact Us", file: "contact_us.html"},
 ];
 
+// Adding buttons with links in the order of the pages constant to the navBar
 const navBar = document.getElementById("navBar")
-
 for (let page of pages) {
     let link = document.createElement("a");
     let button = document.createElement("button");
