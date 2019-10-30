1d9c7f0cd928db6e192535b713937042d6bff1c0
unknown <lukasnt@stud.ntnu.no>
2019-10-30 15:00:03 +0100

    Removed the menuItems in menu.js and used menuItems.js instead. Added else some comments in menu.js

--- Hall's Kitchen/scripts/menu.js
+++ Hall's Kitchen/scripts/menu.js
@@ -1,117 +1,6 @@
-// This is a list of objects that has information and represents the menu items.
-const menuItems = {
-    hamburger: {
-        key: "hamburger",
-        name: "Hamburger",
-        type: "dinner",
-        image_small: "images/menu/hamburger-small.jpg",
-        image_large: "images/menu/hamburger-large.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrudexercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        price: "100",
-        weight: "200g"
-    },
-    pizza: {
-        key: "pizza",
-        name: "Pizza",
-        type: "dinner",
-        image_small: "images/menu/pizza-small.jpg",
-        image_large: "images/menu/pizza-large.jpg",
-        description: "This is a description about Pizza",
-        price: "100",
-        weight: "200g"
-    },
-    steak: {
-        key: "steak",
-        name: "Steak",
-        type: "dinner",
-        image_small: "images/menu/steak-small.jpg",
-        image_large: "images/menu/steak-large.jpg",
-        description: "This is a description about Steak",
-        price: "100",
-        weight: "200g"
-    },
-    noodles: {
-        key: "noodles",
-        name: "Noodles",
-        type: "dinner",
-        image_small: "images/menu/noodles-small.jpg",
-        image_large: "images/menu/noodles-large.jpg",
-        description: "This is a description about Noodles",
-        price: "100",
-        weight: "200g"
-    }, 
-    meatBalls: {
-        key: "meatBalls",
-        name: "Meat Balls",
-        type: "dinner",
-        image_small: "images/menu/meatballs-small.jpg",
-        image_large: "images/menu/meatballs-large.jpg",
-        description: "This is a description about Meat Balls",
-        price: "100",
-        weight: "200g"
-    },
-    iceCream: {
-        key: "iceCream",
-        name: "Ice Cream",
-        type: "dessert",
-        image_small: "images/menu/ice_cream-small.jpg",
-        image_large: "images/menu/ice_cream-large.jpg",
-        description: "This is a description about Ice cream",
-        price: "50",
-        weight: "50g"
-    },
-    chocolateCake: {
-        key: "chocolateCake",
-        name: "Chocolate Cake",
-        type: "dessert",
-        image_small: "images/menu/chocolate_cake-small.jpg",
-        image_large: "images/menu/chocolate_cake-large.jpg",
-        description: "This is a description about Chocolate cake",
-        price: "50",
-        weight: "50g"
-    },
-    milkshake: {
-        key: "milkshake",
-        name: "Milkshake",
-        type: "dessert",
-        image_small: "images/menu/milkshake-small.jpg",
-        image_large: "images/menu/milkshake-large.jpg",
-        description: "This is a description about Milkshake",
-        price: "50",
-        weight: "50g"
-    },
-    springRolls: {
-        key: "springRolls",
-        name: "Spring Rolls",
-        type: "appetizer",
-        image_small: "images/menu/spring_rolls-small.jpg",
-        image_large: "images/menu/spring_rolls-large.jpg",
-        description: "This is a description about Spring Rolls",
-        price: "50",
-        weight: "50g"
-    },
-    frenchFries: {
-        key: "frenchFries",
-        name: "French Fries",
-        type: "appetizer",
-        image_small: "images/menu/french_fires-small.jpg",
-        image_large: "images/menu/french_fires-large.jpg",
-        description: "This is a description about French Fries",
-        price: "50",
-        weight: "50g"
-    },
-    chickenSoup: {
-        key: "chickenSoup",
-        name: "Chicken Soup",
-        type: "appetizer",
-        image_small: "images/menu/chicken_soup-small.jpg",
-        image_large: "images/menu/chicken_soup-large.jpg",
-        description: "This is a description about Chicken Soup",
-        price: "50",
-        weight: "50g"
-    }
-};
-
+/**
+ * This function will generate all the images and buttons for all of the menu-Items in the menuItems array.
+ */
 function generateMenuItems() {
     for (item in menuItems) {
         // Create all elements variables
@@ -136,6 +25,10 @@ function generateMenuItems() {
     }
 }
 
+/**
+ * Creates a div that has the content of a menu-item. This div will be placed inside the window when you select a menu Item.
+ * @param {*} menuItem This is what menu-item that should be made content for in the window.
+ */
 function createWindowDiv(menuItem) {
     // Create all element variables
     let windowDiv = document.createElement("div");
@@ -154,11 +47,14 @@ function createWindowDiv(menuItem) {
     addButton.addEventListener("click", function() {
         addItemToCart(menuItem);
         unFocusWindow();
-
+        
+        // Changing the response window for the approprite menuItem
+        document.getElementById("menuItemText").innerText = menuItem.name;
         let responseDiv = document.getElementById("response");
         responseDiv.style.display = "block";
         focusWindow(responseDiv, false);
     });
+    // This could've been done with a list, but it was just 3 things
     detailText.innerHTML = "Type: " + menuItem.type + "<br> Price:" + menuItem.price + "<br> Weight: " + menuItem.weight;
     detailText.id = "detailText";
 
@@ -171,6 +67,11 @@ function createWindowDiv(menuItem) {
     return windowDiv;
 }
 
+/**
+ * Increments the item in the cart in localStorage.
+ * The cart in localStorage works just like a simple map that has item: amount, as key/value pair.
+ * @param {*} item - The item that should be incrementet.
+ */
 function addItemToCart(item) {
     let cart = JSON.parse(localStorage.getItem("cart"));
     if (cart == null) cart = {};
